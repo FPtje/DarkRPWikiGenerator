cab010b9bde3710bf411aa722de5f8d6a85c649b
Falco Peijnenburg <fpeijnenburg@gmail.com>
2014-01-31 21:53:56 +0100

    Updated to work with latest steam installation site

--- copyscript.sh
+++ copyscript.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-rm -r ~/.local/share/Steam/SteamApps/common/GarrysMod/garrysmod/data/darkrpwiki
+rm -r ~/.steam/steam/SteamApps/common/GarrysMod/garrysmod/data/darkrpwiki
 read -p "Enter darkrp_generatewiki in gmod console
 "
 
 rm -r pages
 mkdir pages
-cp -r ~/.local/share/Steam/SteamApps/common/GarrysMod/garrysmod/data/darkrpwiki/* pages
+cp -r ~/.steam/steam/SteamApps/common/GarrysMod/garrysmod/data/darkrpwiki/* pages
 
 read -p "Press [Enter] to run the upload script
 "
